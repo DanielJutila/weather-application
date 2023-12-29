35b0b5bdafc7ba23f31c0601fd2dd1f7159e300b
Daniel Jutila <daniel.jutila@gmail.com>
2023-12-28 19:00:14 -0600

    Added divs under search bar to show previously searched cities

--- assets/scripts/apiendpoints.js
+++ assets/scripts/apiendpoints.js
@@ -1,3 +1,4 @@
+
 async function getApi(city, state, country){
     let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "," + state + "," + country +'&appid=f8b3ce2dc2fd8e1b0d8f9ba635463824'
     fetch(requestUrl)
@@ -6,27 +7,30 @@ async function getApi(city, state, country){
       })
       .then(function(data){
         console.log(data);
-        saveSearchHistory(data.city.coord.lat, data.city.coord.lon);
+        saveSearchHistory(data.city.coord.lat, data.city.coord.lon, data.city.name);
       });
 }
 
-function searchedCity(){
-    $('#searchBtn').click(function(){
-        let searchValue = $('#searchBar').val();
-        //splits string
-        let partsOfStr = searchValue.split(',');
-        console.log(partsOfStr + "    " + searchValue);
-        if(searchValue !== ""){
-            getApi(partsOfStr[0], partsOfStr[1], partsOfStr[2]);
-        }
+function pressSearched(){
+    $('#searchBtn').on("click", function(){
+        searchingCity();
     })
+}
+function searchingCity(){
+    let searchValue = $('#searchBar').val();
+    //splits string
+    let partsOfStr = searchValue.split(',');
+    console.log(partsOfStr + "    " + searchValue);
+    if(searchValue !== ""){
+        getApi(partsOfStr[0], partsOfStr[1], partsOfStr[2]);
+    }
 }
 
 //saves search history
-function saveSearchHistory(lat, lon){
+function saveSearchHistory(lat, lon, name){
     let searchHistory = getSearchHistory();
     if(lat !== null || lat !== "" || lon !==null || lon !==""){
-        let coords = lat + "," + lon;
+        let coords = lat + "," + lon + "," + name;
         let index = searchHistory.indexOf(coords);
         //removes from list if coords are already present
         if (index !== -1) {
@@ -41,4 +45,4 @@ function getSearchHistory(){
     let searchHistory = localStorage.getItem('searchHistory');
     return searchHistory ? JSON.parse(searchHistory) : [];
 }
-searchedCity();
+pressSearched();
--- assets/scripts/renderWeather.js
+++ assets/scripts/renderWeather.js
@@ -1,1 +1,18 @@
-const getSearch = getSearchHistory();
+$(document).ready(function() {
+let getSearch = getSearchHistory();
+let history = $('#searchHistory');
+let searchSize = 8;
+function previousSearches(){
+    for(let i = 0; i <searchSize; i++){
+        let parts = getSearch[i].split(',');
+        let cityName = parts[2];
+        let newDiv = $("<div>").attr('id','cities').addClass('bg-secondary container m-1 p-2 rounded text-center h5').text(cityName);
+        history.append(newDiv);
+    }
+}
+function clickHistory(){
+    
+}
+previousSearches();
+console.log(getSearch);
+});
