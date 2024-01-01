1349e3248158ba11fc4ba3d89f73c2432fb46e7e
Daniel Jutila <daniel.jutila@gmail.com>
2023-12-31 18:03:21 -0600

    Changed the requestUrl to weather instead of forcast. Now am getting current weather feed

--- assets/scripts/apiendpoints.js
+++ assets/scripts/apiendpoints.js
@@ -1,24 +1,27 @@
 
 function getApi(city, state, country) {
-    let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "," + state + "," + country + '&appid=f8b3ce2dc2fd8e1b0d8f9ba635463824&units=imperial'
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "," + state + "," + country + '&appid=f8b3ce2dc2fd8e1b0d8f9ba635463824&units=imperial'
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            saveSearchHistory(data.city.coord.lat, data.city.coord.lon, data.city.name);
+            console.log(data);
+            saveSearchHistory(data.coord.lat, data.coord.lon, data.name);
             showWeather(data);
+            showFiveDay(data);
         });
 }
 //gets api request from lat and lon
 function getApiGeo(lat, lon) {
-    let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=f8b3ce2dc2fd8e1b0d8f9ba635463824&units=imperial'
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=f8b3ce2dc2fd8e1b0d8f9ba635463824&units=imperial'
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             showWeather(data);
+            showFiveDay(data);
             console.log(data);
         });
 }
@@ -29,18 +32,22 @@ let today = dayjs().format('MMM D, YYYY');
 let currentWeather = $('.currentWeather');
 function showWeather(data) {
     currentWeather.empty();
-    let currentArea = $('<p>').addClass('fw-bold m-2').text(data.city.name + ' (' + today + ')');
-    let icon = data.list[0].weather[0].icon;
+    let currentArea = $('<p>').addClass('fw-bold m-2').text(data.name + ' (' + today + ')');
+    let icon = data.weather[0].icon;
     let img = $('<img>');
     img.attr('src', 'https://openweathermap.org/img/w/' + icon + '.png');
     currentArea.append(img);
-    let currentTemp = $('<p>').addClass('m-2').text('Temp: ' + data.list[0].main.temp + ' °F');
-    let currentHumidity = $('<p>').addClass('m-2').text('Wind: ' + data.list[0].main.humidity + ' %');
-    let currentWind = $('<p>').addClass('m-2').text('Humidity: ' + data.list[0].wind.speed + ' MPH');
+    let currentTemp = $('<p>').addClass('m-2').text('Temp: ' + data.main.temp + ' °F');
+    let currentHumidity = $('<p>').addClass('m-2').text('Wind: ' + data.main.humidity + ' %');
+    let currentWind = $('<p>').addClass('m-2').text('Humidity: ' + data.wind.speed + ' MPH');
     currentWeather.append(currentArea);
     currentWeather.append(currentTemp);
     currentWeather.append(currentHumidity);
     currentWeather.append(currentWind);
+}
+let fiveDay = $('#fiveDay');
+function showFiveDay(data){
+
 }
 
 function pressSearched() {
